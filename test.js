acd7838c4e087e45dca87dfffb0873cc5626aca1
Mmsshakil <softhandshakil@gmail.com>
2023-09-05 04:13:39 +0600

    very important topic

--- test.js
+++ test.js
@@ -1,16 +1,47 @@
-const addProduct = () =>{
-    const productName = document.getElementById('product-name');
-    const product = productName.value;
-    const productQuantity = document.getElementById('product-quantity');
-    const quantity = productQuantity.value;
+const addProduct = () => {
+    const productField = document.getElementById('product-name');
+    const quantityField = document.getElementById('product-quantity');
 
-    productName.value = '';
-    productQuantity.value = '';
-    console.log(`${product} : ${quantity}`);
+    const product = productField.value;
+    const quantity = quantityField.value;
+    productField.value = '';
+    quantityField.value = '';
 
-    const productList = document.getElementById('productList');
-    const li = document.createElement('li');
-    li.innerText = `${product} : ${quantity}`;
-    productList.appendChild(li);
-
+    displayProduct(product, quantity);
+    console.log(product, quantity);
+    saveProductToLocalStorage(product, quantity);
 }
+
+const displayProduct = (product, quantity) => {
+    const ul = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText = `${product}: ${quantity}`;
+
+    ul.appendChild(li);
+}
+
+const getStoredShoppingCart = () => {
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringfy = JSON.stringify(cart);
+    localStorage.setItem("cart", cartStringfy);
+}
+
+const showCart = () => {
+    const cart = getStoredShoppingCart();
+    for (const product in cart) {
+        console.log(product, cart[product]);
+        displayProduct(product , cart[product])
+    }
+}
+
+showCart();
